f84457a145f21b8d5adb7eafe3893188ccd69ee6
Dan Vanderkam <danvdk@gmail.com>
2024-02-06 12:48:27 -0500

    add baselines for inferring type predicates

---
+++ tests/baselines/reference/inferTypePredicateExplicit.js
@@ -0,0 +1,20 @@
+//// [tests/cases/compiler/inferTypePredicateExplicit.ts] ////
+
+//// [inferTypePredicateExplicit.ts]
+function isNonNullExplicit(x: number | null) {
+  const ok = x !== null;
+  if (ok) {
+    let t: number = x;
+  }
+  return ok;
+}
+
+
+//// [inferTypePredicateExplicit.js]
+function isNonNullExplicit(x) {
+    var ok = x !== null;
+    if (ok) {
+        var t = x;
+    }
+    return ok;
+}
---
+++ tests/baselines/reference/inferTypePredicateImplicit.js
@@ -0,0 +1,22 @@
+//// [tests/cases/compiler/inferTypePredicateImplicit.ts] ////
+
+//// [inferTypePredicateImplicit.ts]
+function isNonNull(x: number | null) {
+  const ok = x !== null;
+  return ok;
+}
+
+declare let tNN: number | null;
+if (isNonNull(tNN)) {
+  let t: number = tNN;
+}
+
+
+//// [inferTypePredicateImplicit.js]
+function isNonNull(x) {
+    var ok = x !== null;
+    return ok;
+}
+if (isNonNull(tNN)) {
+    var t = tNN;
+}
