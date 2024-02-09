e68a9d71f93c31c3f38b9bb879ba49094585a548
Dan Vanderkam <danvdk@gmail.com>
2024-02-09 09:31:36 -0500

    flakyIsString

--- tests/cases/compiler/inferTypePredicateImplicit.ts
+++ tests/cases/compiler/inferTypePredicateImplicit.ts
@@ -8,3 +8,9 @@ declare let tNN: number | null;
 if (isNonNull(tNN)) {
   let t: number = tNN;
 }
+
+// Should not infer a type predicate for this function.
+// true return => x is string, but false return !=> x is number
+function flakyIsString(x: string | number) {
+  return typeof x === 'string' && Math.random() > 0.5;
+}
