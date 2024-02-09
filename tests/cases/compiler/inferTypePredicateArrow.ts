e047ec663340e5bf515bef47957fcef01f20a982
Dan Vanderkam <danvdk@gmail.com>
2024-02-09 18:08:32 -0500

    a few more examples

--- tests/cases/compiler/inferTypePredicateArrow.ts
+++ tests/cases/compiler/inferTypePredicateArrow.ts
@@ -16,3 +16,36 @@ const evenSquares: number[] =
 [1, 2, 3, 4]
     .map(x => x % 2 === 0 ? x * x : null)
       .filter(isTruthy);
+
+// Type guards can flow between functions
+const myGuard = (o: string | undefined): o is string => !!o;
+const mySecondGuard = (o: string | undefined) => myGuard(o);
+
+// https://github.com/microsoft/TypeScript/issues/16069#issuecomment-1327449914
+type MyObj = { data?: string };
+type MyArray = { list?: MyObj[] }[];
+const myArray: MyArray = [];
+
+const result = myArray
+  .map((arr) => arr.list)
+  .filter((arr) => arr && arr.length)
+  .map((arr) => arr
+//              ^^^ Object is possibly 'undefined'.
+    .filter((obj) => obj && obj.data)
+    .map(obj => JSON.parse(obj.data))
+//                         ^^^^^^^^ Type 'undefined' is not assignable to type 'string'.
+  );
+
+// https://github.com/microsoft/TypeScript/issues/16069#issuecomment-1335186481
+
+// https://github.com/microsoft/TypeScript/issues/16069#issuecomment-1183547889
+type Foo = {
+  foo: string;
+}
+type Bar = Foo & {
+  bar: string;
+}
+
+const list: (Foo | Bar)[] = [];
+const result = list.filter((value) => 'bar' in value);
+// result type should be `Bar[]`
